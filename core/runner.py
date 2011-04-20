3a2a78c7eaeb3db7d5b9db5ab22deccc6312855d
Richard Marko <rissko@gmail.com>
2011-04-20 18:00:43 +0200

    Input files passing

--- core/runner.py
+++ core/runner.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import logging
 
 class ModuleRunner(object):
@@ -14,17 +15,39 @@ class ModuleRunner(object):
             self.run_single(cls)
         # TODO: spawn low priority
 
+    def get_inputs(self, method):
+        out = []
+        dir_path = getattr(self.cfg, "%s_dir" % method)
+        dir_mask = getattr(self.cfg, "%s_dir_mask" % method)
+        for root, dirs, files in os.walk(dir_path):
+            for name in files:
+                out.append(os.path.join(root,name))
+
+        return out
+
     def run_single(self, cls_tuple):
         cls, params = cls_tuple
         logging.debug('Instantiating class %s', cls)
         to_run = cls()
         tmp_dir_path = os.path.join(self.cfg.tmp_dir, str(to_run))
         logging.debug('[%s] Creating tmp dir [%s]', to_run, tmp_dir_path)
+        if os.path.isdir(tmp_dir_path):
+            shutil.rmtree(tmp_dir_path)
         os.mkdir(tmp_dir_path)
-        logging.debug('[%s] Set up phase', to_run)
-        to_run.set_up(tmp_dir_path, params)
-        logging.debug('[%s] Run phase', to_run)
-        to_run.run()
+
+        inputs = self.get_inputs(to_run.method())
+        for input_file_path in inputs:
+            logging.debug('[%s] Input file: "%s"', to_run, 
+                input_file_path)
+            logging.debug('[%s] Set up phase', to_run)
+            to_run.set_up(input_file_path, tmp_dir_path, params)
+            logging.debug('[%s] Run phase', to_run)
+            to_run.run()
+
+        if inputs == []:
+            logging.warning('No input files for method "%s" found',
+                to_run.method())
+
         logging.debug('[%s] Tear down phase', to_run)
         to_run.tear_down()
         logging.debug('[%s] Remove tmp dir', to_run)
--- mut/zzuf.py
+++ mut/zzuf.py
@@ -2,7 +2,10 @@ class FuzzWrapper(object):
     def __str__(self):
         return 'zzuf'
 
-    def set_up(self, tmp_dir_path, params):
+    def method(self):
+        return 'mut'
+
+    def set_up(self, input_file_path, tmp_dir_path, params):
         pass
 
     def run(self):
