6d86739196e09b996a7c80838097e92bb0e5989b
Richard Marko <rissko@gmail.com>
2011-04-22 22:47:05 +0200

    complete zzuf wrapper

--- mut/zzuf.py
+++ mut/zzuf.py
@@ -14,11 +14,16 @@ class FuzzWrapper(wrapper.DfuzzWrapper):
         self.output = tmp_dir_path
         self.zzuf_params = params
 
+        # TODO (major): parse zzuf_params
+        self.ratio = (.004, .01)
+        self.seed_range = (2, 7)
+
     def run(self):
         out_path = os.path.join(self.output, 'zzuf.cfg')
         def generator():
-            for name in range(10):
-                self.system('zzuf < %s > %s' % (self.input, out_path))
+            for seed in range(*self.seed_range):
+                self.system('zzuf -s%d -r%s < %s > %s' % (seed,
+                    '%s:%s' % self.ratio, self.input, out_path))
                 yield out_path
 
         return generator
