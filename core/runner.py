8ba31a1891a789b7e3d47fdfae869908aa2b7a3a
Richard Marko <rissko@gmail.com>
2011-04-22 18:05:55 +0200

    Rmtree instead of rmdir

--- core/runner.py
+++ core/runner.py
@@ -77,4 +77,4 @@ class ModuleRunner(object):
         logging.debug('[%s] Tear down phase', to_run)
         to_run.tear_down()
         logging.debug('[%s] Remove tmp dir', to_run)
-        os.rmdir(tmp_dir_path)
+        shutil.rmtree(tmp_dir_path)
