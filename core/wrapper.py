4bd81bb92f1c920a4b14221ac32829869f873f95
Richard Marko <rissko@gmail.com>
2011-04-22 22:48:51 +0200

    Wrapper exec logging

--- core/wrapper.py
+++ core/wrapper.py
@@ -6,7 +6,7 @@ class DfuzzWrapper(object):
         return 'undefined wrapper, add __str__ method'
 
     def system(self, command):
-        print 'EXEC: %s' % command
+        logging.debug('Executing: %s' % command)
         retcode = subprocess.call(command, shell=True)
         if retcode != 0:
             logging.error('Command "%s" returns nonzero value '
