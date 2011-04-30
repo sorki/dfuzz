db8e53746087760a489d78ed6f438e58672d2fa7
Richard Marko <rissko@gmail.com>
2011-04-30 19:16:15 +0200

    Simple cp wrapper, mainly for debugging

---
+++ dfuzz/mut/plain.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import logging
+
+from dfuzz.core import wrapper
+
+class FuzzWrapper(wrapper.DfuzzWrapper):
+    def __str__(self):
+        return 'plain'
+
+    def method(self):
+        return 'mut'
+
+    def set_up(self, input_file_path, tmp_dir_path, params):
+        self.input = input_file_path
+        self.output = tmp_dir_path
+        self.params = params
+
+    def run(self):
+        out_path = os.path.join(self.output, 'plain.cfg')
+        def generator():
+            error = False
+            try:
+                shutil.copyfile(self.input, out_path)
+            except IOError as e:
+                logging.error('IO exception: %s', e)
+                error = True
+
+            if error:
+                yield None
+            else:
+                yield out_path
+
+        return generator
+
+    def tear_down(self):
+        pass
