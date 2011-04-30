011f77e16bbd1495ec5e484911a5753b9d9bea08
Richard Marko <rissko@gmail.com>
2011-04-30 23:30:50 +0200

    Modify handler to act as console handler

--- dfuzz/core/handler.py
+++ dfuzz/core/handler.py
@@ -4,15 +4,18 @@ Incident handler
 
 import logging
 
-class IncidentHandler(object):
+class ConsoleIncidentHandler(object):
     def __init__(self, cfg, fuzzer):
         self.cfg = cfg
         self.fuzzer = fuzzer
 
     def handle_failure(self, target_obj, input_file_path):
-        logging.debug('Handling failure')
-        logging.debug('Command line: %s', target_obj.cmd)
-        logging.debug('Target retcode: %d', target_obj.code)
-        logging.debug('Target stdout: %s', target_obj.stdout)
-        logging.debug('Target stderr: %s', target_obj.stderr)
-        logging.debug('Fuzzing method: %s', self.fuzzer)
+        def log_fn(str):
+            logging.warning('[%s] %s' % (self.fuzzer, str))
+
+        log_fn('Handling failure')
+        log_fn('Command line: %s'% target_obj.cmd)
+        log_fn('Target retcode: %d'% target_obj.code)
+        log_fn('Target stdout: %s'% target_obj.stdout)
+        log_fn('Target stderr: %s'% target_obj.stderr)
+        log_fn('Fuzzing method: %s'% self.fuzzer)
