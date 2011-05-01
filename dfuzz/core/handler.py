57d1fa7373ace3a11104db98d4bbc9035c00c785
Richard Marko <rissko@gmail.com>
2011-05-01 13:52:21 +0200

    Improved handler logic, added semaphores for thread safety

--- dfuzz/core/handler.py
+++ dfuzz/core/handler.py
@@ -2,20 +2,42 @@
 Incident handler
 '''
 
+import os
 import logging
 
-class ConsoleIncidentHandler(object):
+from dfuzz.core import utils
+
+
+class CoreIncidentHandler(object):
     def __init__(self, cfg, fuzzer):
         self.cfg = cfg
         self.fuzzer = fuzzer
+        self.cfg.handler_semaphore.acquire()
+
+    def __del__(self):
+        self.cfg.handler_semaphore.release()
 
     def handle_failure(self, target_obj, input_file_path):
+        self.cfg.num_incidents += 1
+        logging.warning('[%s] Handling incident', self.fuzzer)
+
+class ConsoleIncidentHandler(CoreIncidentHandler):
+    def handle_failure(self, target_obj, input_file_path):
+        super(ConsoleIncidentHandler, self).handle_failure(target_obj,
+            input_file_path)
+
         def log_fn(str):
             logging.warning('[%s] %s' % (self.fuzzer, str))
 
-        log_fn('Handling failure')
-        log_fn('Command line: %s'% target_obj.cmd)
-        log_fn('Target retcode: %d'% target_obj.code)
-        log_fn('Target stdout: %s'% target_obj.stdout)
-        log_fn('Target stderr: %s'% target_obj.stderr)
-        log_fn('Fuzzing method: %s'% self.fuzzer)
+        log_fn('Command line: %s'   % target_obj.cmd)
+        log_fn('Target retcode: %d' % target_obj.code)
+        log_fn('Target stdout: %s'  % target_obj.stdout)
+        log_fn('Target stderr: %s'  % target_obj.stderr)
+        log_fn('Fuzzing method: %s' % self.fuzzer)
+
+class FileIncidentHandler(object):
+    def handle_failure(self, target_obj, input_file_path):
+        super(ConsoleIncidentHandler, self).handle_failure(target_obj,
+            input_file_path)
+
+        #os.mkdir(utils.parse_incident_fmt(self.cfg))
