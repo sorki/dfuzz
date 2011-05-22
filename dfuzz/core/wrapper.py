08c23bea19996164648dd5a1b9f8b0a12ce09811
Richard Marko <rissko@gmail.com>
2011-05-22 23:53:27 +0200

    Document wrapper class

--- dfuzz/core/wrapper.py
+++ dfuzz/core/wrapper.py
@@ -4,10 +4,26 @@ import subprocess
 from dfuzz.core import exceptions
 
 class DfuzzWrapper(object):
+    ''' 
+    Abstract class to be used as base for
+    new wrappers or standalone fuzzers
+    '''
+
     def __str__(self):
+        '''
+        New class should override this method.
+        Provides easy identification.
+        '''
         return 'undefined wrapper, add __str__ method'
 
     def system(self, command):
+        '''
+        Safely executes supplied command.
+        If the return code of the command is zero, it will
+        return the output of the command.
+
+        Raises SyscallException in case of nonzero return code.
+        '''
         logging.debug('Executing: %s', command)
         pr = subprocess.Popen(command, shell=True,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
